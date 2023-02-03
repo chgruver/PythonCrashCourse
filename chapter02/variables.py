433fa2c3d6c5b937536ce7a73445969762166880
Charlie Gruver <chgruver@gmail.com>
2023-02-02 19:30:47 -0500

    chapter 2 variables & strings

---
+++ chapter02/variables.py
@@ -0,0 +1,25 @@
+message = "Hello Python Crash Course World! ¥\n"
+print(message)
+
+message = "I'm blue da boo de ba boo die\n"
+print(message)
+
+name = "AdA LoveLace\n"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "Chuck"
+last_name = "Gruver"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+language = "   Python   "
+print(language.rstrip())
+print(language.lstrip())
+print(language.strip())
+
+nostarch_url = "https://nostarch.com"
+
+print(nostarch_url.removeprefix("https://"))
+
